1d6186db2af194a66ab8b1252d2f2c8faf59df0c
anhson <sonanhnguyen003@gmail.com>
2024-05-02 09:45:26 +0700

    u

--- src/js/utilities/loadModelFromDish.js
+++ src/js/utilities/loadModelFromDish.js
@@ -53,7 +53,7 @@ export async function loadAllModels(modelPaths) {
     try {
         const models = [];
         for (const modelPath of modelPaths) {
-            const model = await loadModel(modelPath.path[0]);
+            const model = await loadModel(modelPath);
 
             const boundingBox = new THREE.Box3().setFromObject(model);
             const size = new THREE.Vector3();
